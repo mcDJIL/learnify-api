75e87a6b21992519075af02d71d37c9bd75566b9
mcDJIL <mdjauharil29@gmail.com>
2025-09-16 15:16:15 +0700

    feat: add setActiveLesson route to manage user's active lesson

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/courses/review', [CourseReviewController::class, 'store']);
     Route::post('/courses/enroll', [CourseController::class, 'enroll']);
     Route::post('/courses/start-lesson', [CourseController::class, 'startLesson']);
+        Route::post('/lessons/set-active', [CourseController::class, 'setActiveLesson']);
     Route::post('/courses/complete-lesson', [CourseController::class, 'completeLesson']);
     Route::post('/courses/complete-quiz', [CourseController::class, 'completeQuiz']);
     Route::post('/courses/complete', [CourseController::class, 'completeCourse']);
